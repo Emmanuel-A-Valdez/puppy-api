2df933e5c86f654f93616d89d8f5d23c109eb9c2
Emmanuel-A-Valdez <emmanuel_a_valdez@outlook.com>
2021-05-23 15:33:11 -0600

    Added db connection

--- source/settings.py
+++ source/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
     }
 }
 
+
+
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -94,6 +96,10 @@ DATABASES = {
 #         "PORT": "5432",
 #     }
 # }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
